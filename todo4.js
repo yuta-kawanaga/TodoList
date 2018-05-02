efae6e58ccd6eb17b241aaef9713852364ed5e80
yuta-kawanaga <yuta-kawanaga@hands-lab.com>
2018-05-02 22:01:59 +0900

    first commit

---
+++ todo4.js
@@ -0,0 +1,92 @@
+$(function(){
+
+	let viewEl = $("#viewList");
+	let inputEl = $("#todoInput");
+
+	//HTML読み込み時にストレージから表示
+	let storageList = localStorage["todo.list"];
+	if(storageList){
+		JSON.parse(storageList).forEach(function(itemEl){
+			addTodo(itemEl.text,itemEl.complete);
+		});
+	}
+
+	//Todoを追加する関数
+	function addTodo(text,isComplete){
+		//リストアイテムを作成
+		let liEl = $("<li>");
+		let textEl = $('<span id = "text">').text(text);
+		let checkboxEl = $('<input type = "checkbox">');
+		let deleteEl = $('<button id = "deletebtn">削除</button>')
+
+		//完了の場合
+		if(isComplete){
+			liEl.addClass("complete");
+			checkboxEl.attr("checked",true);
+		}
+
+		//追加する要素の作成
+		liEl.append(checkboxEl).append(textEl).append(deleteEl);
+
+		//リストに追加する
+		viewEl.prepend(liEl);
+
+		//チェックボックスをクリックしたとき
+		checkboxEl.click(function(){
+			if($(this).is(":checked")){
+				liEl.addClass("complete");
+			}
+			else{
+				liEl.removeClass("complete");
+			}
+			//LocalStorageを更新
+			updateStorage();
+		});
+
+		//削除ボタンが押されたとき
+		deleteEl.click(function(){
+			//アラート表示
+			if(window.confirm("削除してもよろしいですか？")){
+				liEl.remove();
+			}
+			//LocalStorageを更新
+			updateStorage();
+		});
+	}
+
+	//フォームを送信したとき
+	$("#todoForm").bind("submit",function(e){
+		//フォームのデフォルトの動きを止める？
+		e.preventDefault();
+
+		//入力文字を取得
+		let text = inputEl.val();
+
+		//<ul>に追加
+		addTodo(text);
+
+		//テキストボックスを空に
+		inputEl.val("");
+
+		//LocalStorageを更新
+		updateStorage();
+	});
+
+	//LocalStorageを更新
+	function updateStorage(){
+		let storage = [];
+
+		//現在のリストを全て取得
+		viewEl.find("li").each(function(){
+			let itemEl = $(this);
+
+			//テキストとチェックボックスの状態を保存
+			storage.push({
+				text:itemEl.find("#text").text(),
+				complete:itemEl.hasClass("complete")
+			});
+		});
+		//文字列にして保存
+		localStorage["todo.list"] = JSON.stringify(storage);
+	}
+});
