79775ae426e824a686c4775295cc273a756255fe
yuta-kawanaga <yuta-kawanaga@hands-lab.com>
2018-05-22 16:54:19 +0900

    Add
    更新

--- public/javascripts/todo.js
+++ public/javascripts/todo.js
@@ -1,91 +1,35 @@
 $(function () {
-	// 表示する場所と入力フォーム
+	// 表示する場所
 	const viewEl = $("#viewList");
-	const inputEl = $("#todoInput");
 
-	// 読み込み時に表示
+	// 読み込み時
 	$.ajax({
 		url: 'http://localhost:3000/all',
 		type: 'GET'
 	})
 		.done((res) => {
-			// 通信成功時の処理を記述
-			// 描画処理
-			//console.log(res);
-
+			// 成功時表示処理
 			for (let i = 0; i < res.length; i++) {
-				let liEl = $("<li>");
-				let textEl = $('<span id = "text" contenteditable="true">').text(res[i].text);
-				let deleteEl = $('<button id = "deletebtn"><i class="far fa-trash-alt fa-3x"></i></button>');
-				let editEl = $('<button id = "editbtn" data-iziModal-open=".iziModal"><i class="far fa-save fa-3x"></i></button>')
 				// 追加する要素の作成
-				liEl.append(textEl).append(editEl).append(deleteEl);
-				viewEl.prepend(liEl);
+				const liEl = $("<li>");
+				const textEl = $('<span id = "text" contenteditable="true">').text(res[i].text);
+				const deleteBtn = createDeleteBtn(res, i, liEl);
+				const saveBtn = createSaveBtn(res, i, liEl);
 
-				// 削除ボタンが押されたとき
-				deleteEl.on("click", function () {
-					// アラート表示
-					if (window.confirm("削除してもよろしいですか？")) {
-						// li　をフェードアウトし LocalStorage から削除
-						liEl.fadeOut(400, function () {
-							liEl.remove();
-							$.ajax({
-								url: '/delete',
-								type: 'DELETE',
-								data: {
-									_id: res[i]._id //削除するデータ
-								}
-							})
-								.done((res) => {
-									// 通信成功時の処理を記述
-									//console.log(res);
-								})
-								.fail(() => {
-									// 通信失敗時の処理を記述
-								});
-						});
-					}
-				});
+				liEl.append(textEl).append(saveBtn).append(deleteBtn);
 
-				// 確定ボタンが押されたとき
-				editEl.on("click", function () {
-					//const textEl = $("#text");
-					//let text = $("#text").text();
-					let text = $(this).parent().text();
-
-					console.log(text);
-					
-					
-					$.ajax({
-						url: '/update',
-						type: 'PUT',
-						data: {
-							_id: res[i]._id,
-							text: text, //更新するデータ
-						}
-					})
-						.done((res) => {
-							// 通信成功時の処理を記述
-							//console.log(res);
-						})
-						.fail(() => {
-							// 通信失敗時の処理を記述
-						});
-					// 保存ダイアログを出す
-					//$(".iziModal").iziModal({ timeout: 900, padding: 5, top: 10 });
-				});
+				viewEl.prepend(liEl).hide().fadeIn(400);
 			}
-
-
-			// for (let i = 0; i < res.length; i++) {
-
-			// }
-		})
-		.fail(() => {
-			// 通信失敗時の処理を記述
 		});
-
 	// フォームを送信したときの処理
+	sendForm();
+});
+
+// 入力内容をajaxで送信
+function sendForm() {
+	// 入力欄
+	const inputEl = $("#todoInput");
+
 	$("#todoForm").on("click", function () {
 		// 入力文字を取得
 		const text = inputEl.val();
@@ -94,7 +38,6 @@ $(function () {
 		if (text === "") {
 			return false;
 		};
-
 
 		// テキストボックスを空に
 		inputEl.val("");
@@ -106,13 +49,51 @@ $(function () {
 				text: text,
 				complete: false
 			}
-		})
-			.done((res) => {
-				// 通信成功時の処理を記述
-				console.log(res);
-			})
-			.fail(() => {
-				// 通信失敗時の処理を記述
-			});
+		});
 	});
-});
+}
+
+
+// 削除ボタンを作る関数
+function createDeleteBtn(res, i, liEl) {
+	const deleteBtn = $('<button id = "deletebtn"><i class="far fa-trash-alt fa-3x"></i></button>');
+	// 削除ボタンが押されたとき
+	deleteBtn.on("click", function () {
+		// アラート表示
+		if (window.confirm("削除してもよろしいですか？")) {
+			liEl.fadeOut(400, function () {
+				liEl.remove();
+				$.ajax({
+					url: '/delete',
+					type: 'DELETE',
+					data: {
+						_id: res[i]._id
+					}
+				});
+			});
+		}
+	});
+	return deleteBtn;
+}
+
+// 保存ボタンを作る関数
+function createSaveBtn(res, i, liEl) {
+	let saveBtn = $('<button id = "editbtn" data-iziModal-open=".iziModal"><i class="far fa-save fa-3x"></i></button>')
+	// 保存ボタンが押されたとき
+	saveBtn.on("click", function () {
+
+		let text = $(this).parent().text();
+
+		$.ajax({
+			url: '/update',
+			type: 'PUT',
+			data: {
+				_id: res[i]._id,
+				text: text //更新するデータ
+			}
+		});
+		// 保存ダイアログを出す
+		alert("保存しました。");
+	});
+	return saveBtn;
+}
